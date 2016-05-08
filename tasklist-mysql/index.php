b8203068556d32196c178ffd0176ea9785fc51a6
Jacky Liu <jacky168@gmail.com>
2016-05-08 15:49:59 +0800

    bug fix

--- tasklist-mysql/index.php
+++ tasklist-mysql/index.php
@@ -45,7 +45,7 @@
 	header('Pragma: no-cache');
 	require_once "getitems.php";
 	require_once "getcategorys.php";
-	//require_once "utility.php";
+	require_once "utility.php";
 	$items = getItems();
 	$categorys = getCategorys();
 	if(!empty($items))
